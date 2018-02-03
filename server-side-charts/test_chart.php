8d2f39d199fb6137f8554102bab50e2cddabdf1b
lasalesi <larsericmueller@gmail.com>
2018-02-03 21:56:00 +0100

    working test chart

--- server-side-charts/test_chart.php
+++ server-side-charts/test_chart.php
@@ -29,11 +29,11 @@
  $myPicture->drawRectangle(0,0,699,229,array("R"=>0,"G"=>0,"B"=>0));
  
  /* Write the chart title */ 
- $myPicture->setFontProperties(array("FontName"=>"../fonts/Forgotte.ttf","FontSize"=>11));
+ $myPicture->setFontProperties(array("FontName"=>"./pChart/fonts/verdana.ttf","FontSize"=>11));
  $myPicture->drawText(150,35,"Average temperature",array("FontSize"=>20,"Align"=>TEXT_ALIGN_BOTTOMMIDDLE));
 
  /* Set the default font */
- $myPicture->setFontProperties(array("FontName"=>"../fonts/pf_arma_five.ttf","FontSize"=>6));
+ $myPicture->setFontProperties(array("FontName"=>"./pChart/fonts/verdana.ttf","FontSize"=>6));
 
  /* Define the chart area */
  $myPicture->setGraphArea(60,40,650,200);
@@ -51,9 +51,6 @@
  /* Write the chart legend */
  $myPicture->drawLegend(540,20,array("Style"=>LEGEND_NOBORDER,"Mode"=>LEGEND_HORIZONTAL));
 
- /* Render the picture (choose the best way) */
- //$myPicture->autoOutput("pictures/example.drawLineChart.simple.png");
- header("Content-Type: image/png");
- //header("Content-Length: " . filesize($name));
+ /* Render the picture */
+ $myPicture->autoOutput();
 
- fpassthru($myPicture);
